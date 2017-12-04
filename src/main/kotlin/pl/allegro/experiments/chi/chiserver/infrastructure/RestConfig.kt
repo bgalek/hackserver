06a2b132982fb116319696c266e75febe3090ddf
Kamil Chmielewski <kamil.chmielewski@allegrogroup.com>
2017-12-04 11:03:39 +0100

    CHIBOX-79 FIX druid broker api connection errors
    RestTemplate keep-alive timeout needs to be consistent
    with the one used in Druid VIP configuration
    if we use VIP between chi-server <-> druid.
    
        > Tomasz Ludwiczak·10:06
        > Kamil, 500ms jest timeout keepalive na Haproxy

--- src/main/kotlin/pl/allegro/experiments/chi/chiserver/infrastructure/RestConfig.kt
+++ src/main/kotlin/pl/allegro/experiments/chi/chiserver/infrastructure/RestConfig.kt
@@ -3,19 +3,22 @@ package pl.allegro.experiments.chi.chiserver.infrastructure
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
-import pl.allegro.tech.common.andamio.spring.client.ClientConnectionConfig
+import pl.allegro.tech.common.andamio.spring.client.OkHttpClientConfig
 import pl.allegro.tech.common.andamio.spring.client.RestTemplateFactory
+
 
 @Configuration
 class RestConfig {
     @Bean
     fun restTemplate(factory: RestTemplateFactory): RestTemplate {
-        val connectionConfig = ClientConnectionConfig.clientConnectionConfig()
-                .withMaxConnections(1)
-                .withMaxConnectionsPerRoute(1)
-                .withSocketTimeout(300)
-                .withConnectionTimeout(300)
+        val connectionConfig = OkHttpClientConfig.okHttpClientConfig()
+                .withMaxIdleConnections(2)
+                .withReadTimeoutMillis(300)
+                .withWriteTimeoutMillis(300)
+                .withConnectionTimeoutMillis(300)
+                .withKeepAliveDurationMillis(500) // TODO: needs LBaaS reference
+                .withRetryOnConnectionFailure(true)
                 .build()
-        return factory.usingApacheHttp().create(connectionConfig)
+        return factory.usingOkHttp().create(connectionConfig)
     }
 }
