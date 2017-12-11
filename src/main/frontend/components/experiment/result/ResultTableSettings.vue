efa302cbbdecb98fcc7cf2ed2871b63f55d2e505
Bartlomiej Beczkowski <bartlomiej.beczkowski@allegrogroup.com>
2017-12-11 13:59:33 +0100

    CHIBOX-1 changing device picker

--- src/main/frontend/components/experiment/result/ResultTableSettings.vue
+++ src/main/frontend/components/experiment/result/ResultTableSettings.vue
@@ -1,44 +1,51 @@
 <template>
-  <v-container fluid>
-    <v-layout row wrap>
-      <v-flex xs4 sm3 md3 lg3 xl3>
-        <v-card flat>
-          <v-card-text>
-            <v-menu>
-              <v-text-field
-                slot="activator"
-                label="Results to day:"
-                v-model="toDate"
-                prepend-icon="event"
-              ></v-text-field>
-              <v-date-picker v-model="toDate" :allowed-dates="allowedDates" no-title scrollable actions>
-                <template slot-scope="{ save, cancel }">
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn flat color="primary" @click="cancel">Cancel</v-btn>
-                    <v-btn flat color="primary" @click="save">OK</v-btn>
-                  </v-card-actions>
-                </template>
-              </v-date-picker>
-            </v-menu>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-
-      <v-flex xs8 sm9 md9 lg9 xl9>
-        <v-card flat>
-          <v-card-text>
-            <v-radio-group v-model="device" row>
-              <v-radio flat id="all" label="All" value="all"></v-radio>
-              <v-radio flat id="desktop" label="Desktop" value="desktop"></v-radio>
-              <v-radio flat id="smartphone" label="Smartphone" value="smartphone"></v-radio>
-              <v-radio flat id="tablet" label="Tablet" value="tablet"></v-radio>
-            </v-radio-group>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout row wrap>
+    <v-flex xs11 sm5>
+      <v-card flat>
+        <v-card-text>
+          <v-menu>
+            <v-text-field
+              slot="activator"
+              label="Results to day:"
+              v-model="toDate"
+              prepend-icon="event"
+            ></v-text-field>
+            <v-date-picker v-model="toDate" :allowed-dates="allowedDates" no-title scrollable actions>
+              <template slot-scope="{ save, cancel }">
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn flat color="primary" @click="cancel">Cancel</v-btn>
+                  <v-btn flat color="primary" @click="save">OK</v-btn>
+                </v-card-actions>
+              </template>
+            </v-date-picker>
+          </v-menu>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-spacer></v-spacer>
+    <v-flex xs11 sm5>
+      <v-card flat>
+        <v-card-text>
+          <p>Device:</p>
+          <v-btn-toggle v-model="device" row>
+            <v-btn flat value="all">
+              All
+            </v-btn>
+            <v-btn flat value="desktop">
+              Desktop
+            </v-btn>
+            <v-btn flat value="smartphone">
+              Smartphone
+            </v-btn>
+            <v-btn flat value="tablet">
+              Tablet
+            </v-btn>
+          </v-btn-toggle>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
