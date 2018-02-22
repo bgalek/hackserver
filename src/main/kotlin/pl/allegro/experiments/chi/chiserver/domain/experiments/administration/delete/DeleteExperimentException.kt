81ec0cf0ef048eb3b474c3b80604ccc172940094
Bartlomiej Beczkowski <bartlomiej.beczkowski@allegro.pl>
2018-02-22 15:19:24 +0100

    CHIBOX-1 DeleteExperimentException to java

--- src/main/kotlin/pl/allegro/experiments/chi/chiserver/domain/experiments/administration/delete/DeleteExperimentException.kt
+++
@@ -1,3 +0,0 @@
-package pl.allegro.experiments.chi.chiserver.domain.experiments.administration.delete
-
-class DeleteExperimentException (override val message: String, override val cause : Exception? = null): RuntimeException(message, cause)
