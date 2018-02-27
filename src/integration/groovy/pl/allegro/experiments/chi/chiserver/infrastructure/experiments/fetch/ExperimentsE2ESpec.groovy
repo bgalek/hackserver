c88ce95b12a1aced985f0f02b3da2eef776e2ddd
Rafal Zukowski <rafal.zukowski@allegro.pl>
2018-02-27 13:43:10 +0100

    CHIBOX-95 Crisis management

--- src/integration/groovy/pl/allegro/experiments/chi/chiserver/infrastructure/experiments/fetch/ExperimentsE2ESpec.groovy
+++ src/integration/groovy/pl/allegro/experiments/chi/chiserver/infrastructure/experiments/fetch/ExperimentsE2ESpec.groovy
@@ -12,12 +12,13 @@ import pl.allegro.experiments.chi.chiserver.BaseIntegrationSpec
 import pl.allegro.experiments.chi.chiserver.domain.User
 import pl.allegro.experiments.chi.chiserver.domain.UserProvider
 import pl.allegro.experiments.chi.chiserver.domain.experiments.ExperimentsRepository
+import pl.allegro.experiments.chi.chiserver.infrastructure.experiments.ExampleExperiments
 import pl.allegro.experiments.chi.chiserver.infrastructure.experiments.ExperimentsDoubleRepository
 import pl.allegro.experiments.chi.chiserver.infrastructure.experiments.FileBasedExperimentsRepository
 import spock.lang.Shared
 
 @DirtiesContext
-class ExperimentsE2ESpec extends BaseIntegrationSpec {
+class ExperimentsE2ESpec extends BaseIntegrationSpec implements ExampleExperiments {
 
     @ClassRule
     @Shared
@@ -225,99 +226,5 @@ class ExperimentsE2ESpec extends BaseIntegrationSpec {
         then:
         def ex = thrown(HttpClientErrorException)
         ex.statusCode.value() == 400
-    }
-
-    Map internalExperiment() {
-        [ id: 'internal_exp',
-          variants: [
-                  [ name: 'internal', predicates: [[type: 'INTERNAL']] ]
-          ],
-          reportingEnabled: false,
-          groups: [],
-          status: 'DRAFT'
-        ]
-    }
-
-    Map cmuidRegexpExperiment() {
-        [ id: 'cmuid_regexp',
-          variants: [
-                  [ name: 'v1', predicates: [[type: 'CMUID_REGEXP', regexp: '.*[0-3]$']] ]
-          ],
-          reportingEnabled: true,
-          description: "Experiment description",
-          author: "Experiment owner",
-          groups: [],
-          status: 'DRAFT'
-        ]
-    }
-
-    Map hashVariantExperiment() {
-        [ id: 'test_dev',
-          variants: [
-                  [ name: 'v1', predicates: [[type: 'HASH', from: 0, to: 50]] ],
-                  [ name: 'v2', predicates: [[type: 'HASH', from: 50, to: 100]] ]
-          ],
-          reportingEnabled: true,
-          groups: [],
-          status: 'DRAFT'
-        ]
-    }
-
-    Map sampleExperiment() {
-        [id: 'another_one',
-         variants: [
-                 [ name: 'v1', predicates: [[type: 'HASH', from: 0, to: 50]] ]
-         ],
-         reportingEnabled: true,
-         description: "Another one",
-         author: "Someone",
-         groups: [],
-         status: 'DRAFT'
-        ]
-    }
-
-    Map timeboundExperiment() {
-        [ id: 'timed_internal_exp',
-          activityPeriod: [
-              activeFrom: '2017-11-03T10:15:30+02:00',
-              activeTo: '2018-11-03T10:15:30+02:00'
-          ],
-          variants: [
-              [ name: 'internal', predicates: [[ type:'INTERNAL' ]] ]
-          ],
-          reportingEnabled: true,
-          groups: [],
-          status: 'ACTIVE'
-        ]
-    }
-
-    Map plannedExperiment() {
-        [ id: 'planned_exp',
-          activityPeriod: [
-                  "activeFrom": "2050-11-03T10:15:30+02:00",
-                  "activeTo": "2060-11-03T10:15:30+02:00"
-          ],
-          variants: [
-              [ name: 'internal', predicates: [[ type:'INTERNAL' ]] ]
-          ],
-          reportingEnabled: true,
-          groups: [],
-          status: 'PLANNED'
-        ]
-    }
-
-    Map experimentFromThePast() {
-        [ id: 'experiment_from_the_past',
-          activityPeriod: [
-              activeFrom: '2017-10-01T10:15:30+02:00',
-              activeTo: '2017-11-01T10:15:30+02:00'
-          ],
-          variants: [
-                  [ name: 'internal', predicates: [[ type:'INTERNAL' ]] ]
-          ],
-          reportingEnabled: true,
-          groups: [],
-          status: 'ENDED'
-        ]
     }
 }
