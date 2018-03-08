a438061e0bd4eb2a5f120f2419a27ca14441bcf3
Bartlomiej Beczkowski <bartlomiej.beczkowski@allegro.pl>
2018-03-08 13:04:39 +0100

    CHIBOX-1 refactoring

--- src/main/java/pl/allegro/experiments/chi/chiserver/application/LogoutController.java
+++ src/main/java/pl/allegro/experiments/chi/chiserver/application/LogoutController.java
@@ -3,7 +3,6 @@ package pl.allegro.experiments.chi.chiserver.application;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
