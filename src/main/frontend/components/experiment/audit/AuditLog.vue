fae451ed64d25edb44bd632c8f59fe1ad78acc0b
Adrian Ziemski <adrian.ziemski@allegro.pl>
2018-03-15 09:58:58 +0100

    CHIBOX-97  Use ChiPanlel to display audit log

--- src/main/frontend/components/experiment/audit/AuditLog.vue
+++ src/main/frontend/components/experiment/audit/AuditLog.vue
@@ -2,26 +2,21 @@
   <v-container>
     <v-layout>
       <v-flex md12 lg10 xl10 offset-xl1 offset-md1 offset-lg>
-        <v-spacer></v-spacer>
-        <v-card>
-          <v-toolbar color="blue" dark>
-            <v-toolbar-title>Audit log for {{ auditLog.experimentId }}</v-toolbar-title>
-          </v-toolbar>
-
-          <v-container grid-list-md>
-            <v-layout row wrap>
-              <template v-for="item in auditLog.changes">
-                <v-flex xs12>
-                  <span>{{ item.formattedDate() }}</span>
-                  <span class="grey--text"> by {{ item.author }}</span>
-                </v-flex>
-                <v-flex xs12>
-                  <code class="changelog">{{ item.changelog }}</code>
-                </v-flex>
-              </template>
-            </v-layout>
-          </v-container>
-        </v-card>
+        <chi-panel :title="panelTitle">
+            <v-container grid-list-md>
+              <v-layout row wrap>
+                <template v-for="item in auditLog.changes">
+                  <v-flex xs12>
+                    <span>{{ item.formattedDate() }}</span>
+                    <span class="grey--text"> by {{ item.author }}</span>
+                  </v-flex>
+                  <v-flex xs12>
+                    <code class="changelog">{{ item.changelog }}</code>
+                  </v-flex>
+                </template>
+              </v-layout>
+            </v-container>
+        </chi-panel>
         <v-alert v-if="errorOccurred()" color="error" icon="warning" value="true">
           Couldn't load audit log: {{ error.message }}
         </v-alert>
@@ -32,17 +27,27 @@
 
 <script>
   import { mapActions, mapState } from 'vuex'
+  import ChiPanel from '../../ChiPanel.vue'
 
   export default {
+    components: {
+      ChiPanel
+    },
     created () {
       const experimentId = this.$route.params.experimentId
       this.getExperimentAuditLog({params: {experimentId: experimentId}})
     },
 
-    computed: mapState({
-      auditLog: state => state.experimentAuditLog.auditLog,
-      error: state => state.experimentAuditLog.error
-    }),
+    computed: {
+      ...mapState({
+        auditLog: state => state.experimentAuditLog.auditLog,
+        error: state => state.experimentAuditLog.error
+      }),
+
+      panelTitle () {
+        return `Audit log for ${this.auditLog.experimentId}`
+      }
+    },
     methods: {
       ...mapActions(['getExperimentAuditLog']),
 
