3df49e1af1ac305881aee85e1cdca96ce324c43d
Bartosz Walacik <bartosz.walacik@allegro.pl>
2018-04-23 10:27:50 +0200

    CHIBOX-1 hot fix

--- src/main/java/pl/allegro/experiments/chi/chiserver/AppRunner.java
+++ src/main/java/pl/allegro/experiments/chi/chiserver/AppRunner.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableScheduling
 public class AppRunner {
     public static void main(String[] args) {
         SpringApplication.run(AppRunner.class, args);
