1e6970a2a1a9a6edb2d0362f518b64659a7a74e8
Bartosz Walacik <bartosz.walacik@allegro.pl>
2018-04-25 13:10:56 +0200

    CHIBOX-1 javadoc

--- src/main/java/pl/allegro/experiments/chi/chiserver/infrastructure/interactions/StatusResourcesController.java
+++ src/main/java/pl/allegro/experiments/chi/chiserver/infrastructure/interactions/StatusResourcesController.java
@@ -8,15 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * //TODO Only for testing Java 10, remove after deploying to PROD
+ */
 @RestController
 @RequestMapping(value = {"/status/resources"}, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+@Deprecated
 class StatusResourcesController {
-  @GetMapping
-  Map<String, Object> getSystemResources() {
-    Runtime runtime = Runtime.getRuntime();
-    Map<String, Object> systemResources = new HashMap<>();
-    systemResources.put("cpu", runtime.availableProcessors());
-    systemResources.put("mem", runtime.maxMemory());
-    return systemResources;
-  }
+    @GetMapping
+    Map<String, Object> getSystemResources() {
+        Runtime runtime = Runtime.getRuntime();
+        Map<String, Object> systemResources = new HashMap<>();
+        systemResources.put("cpu", runtime.availableProcessors());
+        systemResources.put("mem", runtime.maxMemory());
+        return systemResources;
+    }
 }
