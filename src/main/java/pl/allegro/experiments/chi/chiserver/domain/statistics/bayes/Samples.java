1c91506799bafe8695b5e83391520a0e074e4ac8
Rafal Zukowski <rafal.zukowski@allegro.pl>
2018-05-07 23:18:19 +0200

    CHIBOX-106 labels

--- src/main/java/pl/allegro/experiments/chi/chiserver/domain/statistics/bayes/Samples.java
+++ src/main/java/pl/allegro/experiments/chi/chiserver/domain/statistics/bayes/Samples.java
@@ -7,14 +7,20 @@ import com.google.common.base.Preconditions;
 public class Samples {
     private double[] values;
     private int[] counts;
+    private String[] labels;
 
     @JsonCreator
-    public Samples(@JsonProperty("values") double[] values, @JsonProperty("counts") int[] counts) {
+    public Samples(@JsonProperty("values") double[] values,
+                   @JsonProperty("counts") int[] counts,
+                   @JsonProperty("labels") String[] labels) {
         Preconditions.checkNotNull(values, "values should not be null");
         Preconditions.checkNotNull(counts, "counts should not be null");
+        Preconditions.checkNotNull(labels, "labels should not be null");
         Preconditions.checkArgument(values.length == counts.length, "values and counts should be the same length");
+        Preconditions.checkArgument(labels.length == counts.length, "labels and counts should be the same length");
         this.values = values;
         this.counts = counts;
+        this.labels = labels;
     }
 
     public double[] getValues() {
@@ -24,4 +30,6 @@ public class Samples {
     public int[] getCounts() {
         return counts;
     }
+
+    public String[] getLabels() { return labels; }
 }
