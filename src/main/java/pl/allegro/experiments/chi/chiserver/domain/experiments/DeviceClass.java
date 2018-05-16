1fa52fd80837269e7b739c5cb0dcccc6143d5bf6
Bartosz Walacik <bartosz.walacik@allegro.pl>
2018-05-16 08:36:19 +0200

    CHIBOX-1 -smartphone

--- src/main/java/pl/allegro/experiments/chi/chiserver/domain/experiments/DeviceClass.java
+++ src/main/java/pl/allegro/experiments/chi/chiserver/domain/experiments/DeviceClass.java
@@ -1,9 +1,7 @@
 package pl.allegro.experiments.chi.chiserver.domain.experiments;
 
 public enum DeviceClass {
-    //TODO remove phone or smartphone
     phone,
-    smartphone,
     desktop,
     tablet,
     all;
