24ca0b7e431ae108e72453f77381ee073c22f26c
Bartosz Walacik <bartosz.walacik@allegro.pl>
2018-11-21 17:53:50 +0100

    CHIBOX-1 .antMatchers("/", "/login**","/callback/", "/error**").permitAll()

--- src/main/java/pl/allegro/experiments/chi/chiserver/infrastructure/WebSecurityConfig.java
+++ src/main/java/pl/allegro/experiments/chi/chiserver/infrastructure/WebSecurityConfig.java
@@ -32,8 +32,8 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         if (oauthEnabled) {
             http.authorizeRequests()
+                    .antMatchers("/", "/login**","/callback/", "/error**").permitAll()
                     .antMatchers(HttpMethod.GET, "/api/**/*", "/status/**/*", "/explicitStatus/**/*", "/env/**/*").permitAll()
-                    .antMatchers("/login**","/callback/").permitAll()
                     .antMatchers(HttpMethod.POST, "/api/**/*").permitAll()
                     .and().authorizeRequests().anyRequest().authenticated()
                     .and().logout().logoutSuccessUrl("/after-logout").permitAll();
