f55841c2addfcb501d142dcd821e76fcc6da254a
Bartosz Galek <bartosz.galek@allegro.pl>
2018-12-17 14:37:50 +0100

    gradle kotlin dsl

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.1.1.RELEASE")
+    }
+}
+
+apply(plugin = "org.springframework.boot")
+apply(plugin = "io.spring.dependency-management")
+
+plugins {
+    java
+    application
+    idea
+}
+
+application {
+    mainClassName = "pl.allegro.tech.leaders.HackathonServer"
+}
+
+group = "pl.allegro.tech.leaders"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.retry:spring-retry")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus:latest.release")
+    runtimeOnly("de.flapdoodle.embed:de.flapdoodle.embed.mongo:2.2.0")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+    compileOnly("org.springframework.boot:spring-boot-configuration-processor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
