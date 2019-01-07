2998c019778303151ca99cd732b3653822bc9b0a
Bartosz Walacik <bartosz.walacik@allegro.pl>
2019-01-07 10:35:25 +0100

    CHIBOX-143 missing test

--- src/integration/groovy/pl/allegro/experiments/chi/chiserver/infrastructure/experiments/MongoExperimentsRepositoryIntegrationSpec.groovy
+++ src/integration/groovy/pl/allegro/experiments/chi/chiserver/infrastructure/experiments/MongoExperimentsRepositoryIntegrationSpec.groovy
@@ -5,9 +5,11 @@ import org.javers.common.exception.JaversException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.MongoTemplate
 import pl.allegro.experiments.chi.chiserver.BaseIntegrationSpec
+import pl.allegro.experiments.chi.chiserver.domain.experiments.CustomMetricDefinition
 import pl.allegro.experiments.chi.chiserver.domain.experiments.CustomParameter
 import pl.allegro.experiments.chi.chiserver.domain.experiments.DeviceClass
 import pl.allegro.experiments.chi.chiserver.domain.experiments.EventDefinition
+import pl.allegro.experiments.chi.chiserver.domain.experiments.EventDefinitionForVariant
 import pl.allegro.experiments.chi.chiserver.domain.experiments.ExperimentGoal
 import pl.allegro.experiments.chi.chiserver.domain.experiments.ExperimentStatus
 import pl.allegro.experiments.chi.chiserver.domain.experiments.ExperimentsRepository
@@ -55,6 +57,13 @@ class MongoExperimentsRepositoryIntegrationSpec extends BaseIntegrationSpec {
                 .reportingDefinition(new ReportingDefinition([new EventDefinition('c','a','v','l','b')], ReportingType.FRONTEND))
                 .deviceClass(DeviceClass.phone_android)
                 .customParameter(new CustomParameter('k','v'))
+                .customMetricsDefinition(new CustomMetricDefinition('CTR',[
+                    new EventDefinitionForVariant('base', new EventDefinition('c_base_view','a_base_view','v_base_view','l_base_view','b_base_view'),
+                                                          new EventDefinition('c_base_success','a_base_success','v_base_success','l_base_success','b_base_success'))
+                    ,
+                    new EventDefinitionForVariant('v1', new EventDefinition('c_v1_view','a_v1_view','v_v1_view','l_v1_view','b_v1_view'),
+                                                        new EventDefinition('c_v1_success','a_v1_success','v_v1_success','l_v1_success','b_v1_success'))
+                ]))
                 .goal(new ExperimentGoal.Hypothesis("tx_visit", 2),
                       new ExperimentGoal.TestConfiguration(5, 0.05, 0.8, 100_000, 44))
                 .build()
@@ -85,6 +94,17 @@ class MongoExperimentsRepositoryIntegrationSpec extends BaseIntegrationSpec {
         loaded.goal.get().testConfiguration.get().requiredSampleSize == 100_000
         loaded.goal.get().testConfiguration.get().testAlpha == 0.05
         loaded.goal.get().testConfiguration.get().testPower == 0.8
+        loaded.customMetricsDefinition.name == 'CTR'
+        with(loaded.customMetricsDefinition.definitionForVariant[0]) {
+            variantName == 'base'
+            viewEventDefinition == new EventDefinition('c_base_view','a_base_view','v_base_view','l_base_view','b_base_view')
+            successEventDefinition == new EventDefinition('c_base_success','a_base_success','v_base_success','l_base_success','b_base_success')
+        }
+        with(loaded.customMetricsDefinition.definitionForVariant[1]) {
+            variantName == 'v1'
+            viewEventDefinition == new EventDefinition('c_v1_view','a_v1_view','v_v1_view','l_v1_view','b_v1_view')
+            successEventDefinition == new EventDefinition('c_v1_success','a_v1_success','v_v1_success','l_v1_success','b_v1_success')
+        }
 
         when:
         Document doc = mongoTemplate.findById(experiment.id, Document, "experimentDefinitions")
@@ -124,6 +144,35 @@ class MongoExperimentsRepositoryIntegrationSpec extends BaseIntegrationSpec {
             assert it.getString ('testAlpha') == '0.05'
             assert it.getString ('testPower') == '0.80'
             assert it.get ('currentSampleSize') == 44
+        }
+        with(doc.get('customMetricsDefinition')) {
+            assert it.metricName == 'CTR'
+            with(it.get('definitionForVariant')[0]) {
+                it.variantName == 'base'
+                it.get ('viewEventDefinition')['category'] == 'c_base_view'
+                it.get ('viewEventDefinition')['action'] ==   'a_base_view'
+                it.get ('viewEventDefinition')['value'] ==    'v_base_view'
+                it.get ('viewEventDefinition')['label'] ==    'l_base_view'
+                it.get ('viewEventDefinition')['boxName'] ==  'b_base_view'
+                it.get ('successEventDefinition')['category'] == 'c_base_success'
+                it.get ('successEventDefinition')['action'] ==   'a_base_success'
+                it.get ('successEventDefinition')['value'] ==    'v_base_success'
+                it.get ('successEventDefinition')['label'] ==    'l_base_success'
+                it.get ('successEventDefinition')['boxName'] ==  'b_base_success'
+            }
+            with(it.get('definitionForVariant')[1]) {
+                it.variantName == 'v1'
+                it.get ('viewEventDefinition')['category'] == 'c_v1_view'
+                it.get ('viewEventDefinition')['action'] ==   'a_v1_view'
+                it.get ('viewEventDefinition')['value'] ==    'v_v1_view'
+                it.get ('viewEventDefinition')['label'] ==    'l_v1_view'
+                it.get ('viewEventDefinition')['boxName'] ==  'b_v1_view'
+                it.get ('successEventDefinition')['category'] == 'c_v1_success'
+                it.get ('successEventDefinition')['action'] ==   'a_v1_success'
+                it.get ('successEventDefinition')['value'] ==    'v_v1_success'
+                it.get ('successEventDefinition')['label'] ==    'l_v1_success'
+                it.get ('successEventDefinition')['boxName'] ==  'b_v1_success'
+            }
         }
     }
 
