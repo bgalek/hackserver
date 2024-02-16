45f610b844b896ef9588c8f71e526e93a72429a4
Bartosz Gałek <bartosz@galek.com.pl>
2024-02-16 11:10:15 +0100

    Update RegisteredTeamResponse.java

--- src/main/java/pl/allegro/tech/leaders/hackathon/registration/infrastructure/web/RegisteredTeamResponse.java
+++ src/main/java/pl/allegro/tech/leaders/hackathon/registration/infrastructure/web/RegisteredTeamResponse.java
@@ -27,7 +27,7 @@ class RegisteredTeamResponse {
     }
 
     public String getAvatar() {
-        return String.format("https://avatars.dicebear.com/api/male/%s%%40%s.svg", name, address);
+        return String.format("https://api.dicebear.com/7.x/pixel-art/svg?seed=%s%%40%s.svg", name, address);
     }
 
     public boolean getHealth() {
