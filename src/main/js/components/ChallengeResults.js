3b5f1cc168ac3da7ea7745bd10b4d929fa97e890
Bartosz Gałek <bartosz@galek.com.pl>
2024-04-10 11:59:18 +0200

    Update ChallengeResults.js

--- src/main/js/components/ChallengeResults.js
+++ src/main/js/components/ChallengeResults.js
@@ -65,7 +65,7 @@ function renderScore(score, value) {
     if(score){
         return <Chip style={{ color: "white", backgroundColor: green.A700 }} label={value} icon={<DoneIcon style={{ color: "white" }}/>}/>;
     }
-    return <Chip style={{ color: "white", backgroundColor: red.A200 }} label={value || 'error'} icon={<ErrorIcon style={{ color: "white" }}/>}/>;
+    return <Chip style={{ color: "white", backgroundColor: red.A200 }} label={value ? value.substr(0, 20) : 'error'} icon={<ErrorIcon style={{ color: "white" }}/>}/>;
 }
 
 ChallengeResults.propTypes = {
